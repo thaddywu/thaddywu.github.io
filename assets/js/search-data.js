5481ab23257251d599e989431d90d142266093b9
thaddywu <thaddywu@gmail.com>
2025-02-01 09:30:15 +0000

    Deploying to gh-pages from @ thaddywu/thaddywu.github.io@6299c6f0e28096d9f4cfd57351c88a933150ae28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "Publications",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{
+  },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
